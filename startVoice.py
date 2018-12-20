3f6e8bf611a98bf6a20cb606592440cc8ef9ce81
Theriley106 <christopherlambert106@gmail.com>
2018-12-20 16:39:11 -0500

    Added threaded w/ chrome headless driver

--- startVoice.py
+++ startVoice.py
@@ -11,14 +11,15 @@ import copy
 import json
 import threading
 import random
+from selenium.webdriver.chrome.options import Options
 SIM_URL = "https://developer.amazon.com/alexa/console/ask/test/amzn1.ask.skill.a00d7304-d36b-4ae0-8352-dd7e8fbc5d79/development/en_US/"
 COOKIES = "myCookies.json"
 COOKIES = json.load(open(COOKIES))
 ACTIVE_SIMS = []
 ALL_SIMS = []
 lock = threading.Lock()
-THREADS = 5
-ANSWERS = []
+THREADS = 10
+ANSWERS = {}
 class simulator(object):
 	"""docstring for simulator"""
 	def login(self):
@@ -42,6 +43,7 @@ class simulator(object):
 				break
 			count += 1
 			time.sleep(3)
+			self.driver.save_screenshot("{}.png".format(random.randint(1, 50)))
 		if count > 8:
 			return False
 		return True
@@ -58,22 +60,31 @@ class simulator(object):
 		self.driver.find_element_by_css_selector("input.askt-utterance__input").send_keys(question)
 		self.driver.find_element_by_css_selector("input.askt-utterance__input").send_keys(Keys.ENTER)
 		page_source = self.driver.page_source
+		a = 0
 		while "askt-dialog__message askt-dialog__message--spinner" in str(page_source):
-			print("Waiting on load")
+			print("Waiting on question: {}".format(question))
 			time.sleep(.1)
 			page_source = self.driver.page_source
+			a += 1
+			if a > 100:
+				break
 		for i in range(30):
 			x = self.get_response()
 			if x != None:
-				ANSWERS.append(x)
+				ANSWERS[question] = x
 				return
 			time.sleep(.1)
-		ANSWERS.append("I'm not really sure about that.")
+		ANSWERS[question] = "I'm not really sure about that."
 		return
 
 	def __init__(self):
+		options = Options()
+		options.add_argument('--headless')
+		options.add_argument('--disable-gpu')
+		options.add_argument("--mute-audio")
 		self.id = ''.join([str(random.randint(1,9)) for i in range(10)])
-		self.driver = webdriver.Firefox()
+		self.driver = webdriver.Chrome(chrome_options=options)
+		self.driver.set_window_size(1920, 1080)
 		self.login()
 		if self.test_driver() == False:
 			raise Exception("Error on driver...")
@@ -103,28 +114,26 @@ def ask_question(question):
 	mySim.ask_question(question)
 	ACTIVE_SIMS.remove(sim.id)
 
-def printVal(val):
-	print val
-
 def ask_questions(listOfQuestions):
-	while len(ANSWERS) > 0:
-		ANSWERS.pop()
 	threads = [threading.Thread(target=ask_question, args=(arg,)) for arg in listOfQuestions]
 	for thread in threads:
 		thread.start()
 	for thread in threads:
 		thread.join()
-	return ANSWERS
+	return [ANSWERS[question] for question in listOfQuestions]
 
 from flask import Flask, Response, request, jsonify
 app = Flask(__name__)
 
 @app.route('/interact', methods=['POST'])
 def main():
-	response = ask_questions(request.form.getlist('question'))
-	return jsonify({"response": response})
+	start = time.time()
+	questions = request.form.getlist('question')
+	response = ask_questions(questions)
+	end = time.time()
+	return jsonify({"response": response, "time": end - start})
 
 if __name__ == "__main__":
     setup(THREADS)
-    app.run(port=8001)
+    app.run(port=8001, threaded=True)
 
